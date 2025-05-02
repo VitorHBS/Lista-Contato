8a9dcd08111d338e1c141271ec19ee9f5a8fb225
Vitor Barbosa <143558773+VitorHBS@users.noreply.github.com>
2025-05-02 11:33:35 -0300

    Creating server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import helmet from "helmet";
+import router from "./routes";
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(express.urlencoded( {extended: true} ));
+
+server.use('/', router);
+
+server.listen(3000, () => {
+    console.log("Servidor Rodando: https://localhost:3000");
+});
+
+
+
